6753e2850319af248e6a2fa69f23094ae568dfcd
Honza Rychnovský <honzar@appsdevteam.com>
2017-10-27 22:33:36 +0200

    Release keystore

--- app/src/main/java/com/github/hiteshsondhi88/sampleffmpeg/MainActivity.java
+++ app/src/main/java/com/github/hiteshsondhi88/sampleffmpeg/MainActivity.java
@@ -2,11 +2,22 @@ package com.github.hiteshsondhi88.sampleffmpeg;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.github.hiteshsondhi88.libffmpeg.FFmpeg;
 
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        try {
+            FFmpeg ffmpeg = FFmpeg.getInstance();
+            ffmpeg.loadBinary(null,
+                    "", "");
+        } catch (Exception e) {
+            Log.d("ddd", "ddd");
+        }
     }
 }
